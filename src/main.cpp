8f3ee09e68eea4d71aa94a6099b3514b098f5da8
cgn00 <gn.chris00@gmail.com>
2023-03-05 10:25:08 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#define out 11
+
+/// @brief 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(out, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
